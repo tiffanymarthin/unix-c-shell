f9c93f8eb6c71b5729ecebd0608cd2cf8c8aa910
Tiffany <marthin.t@northeastern.edu>
2021-07-30 10:41:49 -0700

    Delete unneccessary libraries

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <sys/types.h>
 #include "shell.h"
 #include "shell_commands.h"
 
